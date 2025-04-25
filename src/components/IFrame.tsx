e4fc74ac9c7d13eb52523aeeab8279a10e132ff9
ramaID <rama@qisthi.dev>
2025-04-25 07:13:59 +0700

    fix: className

--- src/components/IFrame.tsx
+++ src/components/IFrame.tsx
@@ -1,6 +1,6 @@
 export function IFrame(props: React.ComponentProps<'iframe'>) {
   return (
-    <div className="not-prose my-12 grid grid-cols-1 gap-6 sm:grid-cols-2">
+    <div className="w-full">
       <iframe {...props} />
     </div>
   )
